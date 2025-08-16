c108f16eb610802d0eb2e318040aef68a93453c1
AakashN2k03 <aakashvec@gmail.com>
2025-08-16 17:08:33 +0530

    v4 commit

--- tests/test_app.py
+++ tests/test_app.py
@@ -4,11 +4,5 @@ def test_add():
     assert add(2, 3) == 5
     assert add(-1, 1) == 0
     print("All tests passed!")
-from src.app import add
-
-def test_add():
-    assert add(2, 3) == 5
-    assert add(-1, 1) == 0
-    print("All tests passed!")
-    
+ 
 test_add()
